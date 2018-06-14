54acee4386892cc0320b7953c12ea777ec1f4d08
Unknown <satyadara69@gmail.com>
2018-06-14 13:40:01 +0700

    Back to Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String hello_world;
-        int number;
-        double percentage;
-
-        hello_world = "Hello World !";
-        number = 10;
-        percentage = 0.8;
-
-        System.out.println(hello_world + number);
-        System.out.println(percentage);
+        System.out.println("Hello World !");
     }
 }
