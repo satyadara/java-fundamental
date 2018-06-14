363a88395bf04ffbf86334ffdb9756e00c8fe709
Unknown <satyadara69@gmail.com>
2018-06-14 13:51:43 +0700

    Add Class Student

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Student student1 = new Student();
+        student1.setName("Satya");
+        student1.setStudentId("1001");
+
+        System.out.println("name : " + student1.getName());
+        System.out.println("std_ID : " + student1.getStudentId());
+
+        Student student2 = new Student("Megan", "2002");
+
+        student2.setName("Megan Update");
+
+        System.out.println("name : " + student2.getName());
+        System.out.println("std_ID : " + student2.getStudentId());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private String name;
+    private String studentId;
+
+    public Student() {
+    }
+
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+}
